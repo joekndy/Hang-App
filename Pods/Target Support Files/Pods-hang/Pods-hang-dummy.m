7375c59761d45b9584f3473c3e10db06a929d43d
Joe Kennedy <joe@ml.to>
2018-04-16 10:52:13 -0400

    Pods added
    Firebase pods installed

---
+++ Pods/Target Support Files/Pods-hang/Pods-hang-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hang : NSObject
+@end
+@implementation PodsDummy_Pods_hang
+@end
---
+++ Pods/Target Support Files/Pods-hangTests/Pods-hangTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hangTests : NSObject
+@end
+@implementation PodsDummy_Pods_hangTests
+@end
