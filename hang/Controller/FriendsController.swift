bfe1cc80b00e7ff11190b88fa7f7f1df96db3b31
Joe Kennedy <joe@ml.to>
2018-04-18 11:21:45 -0400

    users in table

--- hang/Controller/FriendsController.swift
+++ hang/Controller/FriendsController.swift
@@ -11,6 +11,9 @@ import Firebase
 
 class
 FriendsController: UITableViewController {
+    
+    let cellid = "cellid"
+    var users = [Users]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +21,50 @@ FriendsController: UITableViewController {
         //adds logout item to left of navigation controller
         navigationItem.leftBarButtonItem = UIBarButtonItem(title:"logout", style: .plain, target: self, action: #selector(handleLogout))
         
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellid)
         
+        fetchUser()
+    }
+    
+    func fetchUser() {
+        let rootRef = Database.database().reference()
+        let query = rootRef.child("users").queryOrdered(byChild: "name")
+        query.observe(.value) { (snapshot) in
+            for child in snapshot.children.allObjects as! [DataSnapshot] {
+                if let value = child.value as? NSDictionary {
+                    let user = Users()
+                    let name = value["name"] as? String ?? "Name not found"
+                    let email = value["email"] as? String ?? "Email not found"
+                    user.name = name
+                    user.email = email
+                    self.users.append(user)
+                    DispatchQueue.main.async { self.tableView.reloadData() }
+                }
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //hack for now
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellid, for: indexPath)
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        return cell
+    }
+    
+    class UserCell: UITableViewCell {
+        override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+            super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("")
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
